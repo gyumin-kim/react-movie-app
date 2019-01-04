24c23a2b4b91429c85833a2a52b66601d27ab8d5
Gyumin Kim <starkying@gmail.com>
2019-01-04 20:54:33 +0900

    Updates

---
+++ precache-manifest.4033116f434c883c50629b83fae81f19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "053ee96d5bda23d43b52",
+    "url": "/react-movie-app/static/css/main.ff6a4fcf.chunk.css"
+  },
+  {
+    "revision": "053ee96d5bda23d43b52",
+    "url": "/react-movie-app/static/js/main.053ee96d.chunk.js"
+  },
+  {
+    "revision": "d120fdf0ddba1865b329",
+    "url": "/react-movie-app/static/js/1.d120fdf0.chunk.js"
+  },
+  {
+    "revision": "bdcfacafbc62971f5461",
+    "url": "/react-movie-app/static/js/runtime~main.bdcfacaf.js"
+  },
+  {
+    "revision": "e746578fd5b8c505dc181be0f0086f96",
+    "url": "/react-movie-app/index.html"
+  }
+];
